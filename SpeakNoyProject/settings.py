afdb8ca49d1bd8157f938c2e2c672cea3cfe9109
Keithlog <161996366+Keithlog@users.noreply.github.com>
2026-02-26 23:16:31 +0800

    bug fixed in settings.py

--- SpeakNoyProject/settings.py
+++ SpeakNoyProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
