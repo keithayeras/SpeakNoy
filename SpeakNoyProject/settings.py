3e146d270afedef0f94cedf1fc2332b32279e5b4
Keithlog <161996366+Keithlog@users.noreply.github.com>
2026-02-26 23:41:03 +0800

    fixed more bugs in settings.py and made migrations

--- SpeakNoyProject/settings.py
+++ SpeakNoyProject/settings.py
@@ -17,6 +17,7 @@ import os
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
     "accounts.apps.AccountsConfig",
 ]
 
