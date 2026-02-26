3ff5e5331ab0e0f5bc71cfb0ded60b13fcbe4f7c
Keithlog <161996366+Keithlog@users.noreply.github.com>
2026-02-26 22:54:59 +0800

    created accounts app for handling account systems feature

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = SignUpForm()
+    return render(request, "accounts/signup.html", {"form": form})
+
+@login_required
+def dashboard(request):
+    return render(request, "dashboard.html")
