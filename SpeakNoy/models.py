d034c16a2b2b6ed2186062d3340123a2e3698284
enzo1013 <enzopamintuan.1013@gmail.com>
2026-02-26 18:28:36 +0800

    Added flashcard model

--- SpeakNoy/models.py
+++ SpeakNoy/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Flashcard(models.Model):
+    PURPOSE_CHOICES = [
+        ("noun", "Noun"),
+        ("pronoun", "Pronoun"),
+        ("verb", "Verb"),
+        ("adjective", "Adjective"),
+        ("adverb", "Adverb"),
+    ]
+
+    word = models.CharField(max_length=63)
+    pronunciation = models.CharField(max_length=63)
+    definition = models.TextField()
+    purpose = models.CharField(
+        max_length=20,
+        choices=PURPOSE_CHOICES,
+        default="noun"
+    )
+
+    def __str__(self):
+        return self.word
