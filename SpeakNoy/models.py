4c52b1f537cc7c3fc1678776497fd16e1ffac76f
enzo1013 <enzopamintuan.1013@gmail.com>
2026-02-27 19:00:23 +0800

    Merge main and feature/flashcards, updated db.sqlite3

--- SpeakNoy/models.py
+++ SpeakNoy/models.py
@@ -16,7 +16,7 @@ class Flashcard(models.Model):
     purpose = models.CharField(
         max_length=20,
         choices=PURPOSE_CHOICES,
-        default="noun"
+        default="Noun"
     )
 
     def __str__(self):
