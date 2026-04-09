10534272eb73e206699da4158ddd42e385c56374
enzo1013 <enzopamintuan.1013@gmail.com>
2026-04-09 10:13:01 +0800

    Added urls for card collection

--- SpeakNoy/urls.py
+++ SpeakNoy/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path("review/all/", dialect_review_view, name="dialect_review"),
     path("addcard/", flashcard_create_view, name="cardcreate"),
     path("removecard/<int:pk>/", flashcard_remove, name="cardremove"),
+    path('collections/', collection_list_view, name='collection_list'),
+    path('collections/new/', collection_create_view, name='collection_create'),
+    path('collections/<int:pk>/', collection_detail_view, name='collection_detail'),
+    path('<int:pk>/add-to-collection/', collection_add_card, name='add_to_collection'),
 ]
 
 app_name = 'SpeakNoy'
